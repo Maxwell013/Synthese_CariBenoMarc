41f975834a611bebb267981c417f7e25b49a79ee
Kytih <sckyith@gmail.com>
2024-11-26 14:45:34 -0500

    Script Player v1
    "script player v1" v1

--- Assets/_MyAssets/Scripts/Game/Player.cs
+++ Assets/_MyAssets/Scripts/Game/Player.cs
@@ -1,14 +1,65 @@
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.Scripting.APIUpdating;
 
 public class Player : Entity
 {
-    void Start()
+
+    [SerializeField] private float _delai = 0.5f;
+
+    [SerializeField] private float m_speed;
+    private Rigidbody2D m_rb;
+    
+    private float _cadenceFire = -1; // modifier avec l'animation
+
+
+    private void Start()
     {
-        
+        transform.position = new Vector3(0f, 0f, 0f); // a modifier pour spawn player
     }
 
-    void Update()
+    private void Awake()
     {
-        
+        m_rb = GetComponent<Rigidbody2D>();
+        // m_speed = GameManager.Instance.GetSometginh();
+        m_speed = 1.0f;
     }
+
+    private void FixedUpdate()
+    {
+        Movement();
+    }
+
+    private void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > _cadenceFire)
+        {
+            Fire();
+        }
+
+        Dash();
+    }
+
+    private void Fire()
+    {
+        _cadenceFire = Time.time + _delai;
+        // Instantiate(_laserPrefab, (transform.position + new Vector3(0f, 0.9f, 0f)), Quaternion.identity); // Changer nom prefab en fonction du nom
+    }
+
+    private void Movement()
+    {
+        float positionX = Input.GetAxis("Horizontal");
+        float positionY = Input.GetAxis("Vertical");
+
+        Vector3 direction = new (positionX, positionY, 0f);
+
+        m_rb.AddForce(m_speed * Time.fixedDeltaTime * direction.normalized);
+    }
+
+    private void Dash()
+    {
+       //TODO
+    }
+
 }
